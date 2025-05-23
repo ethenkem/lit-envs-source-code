abad27d0653662a2e9704c782a53ef37c444f6ff
ethenkem <ethenatx@gmail.com>
2025-05-23 15:10:38 +0100

    i am a teapot

---
+++ backend/gitforenv/src/main/java/com/bookmie/gitforenv/GitforenvApplication.java
@@ -0,0 +1,13 @@
+package com.bookmie.gitforenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitforenvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitforenvApplication.class, args);
+	}
+
+}
---
+++ backend/gitforenv/src/test/java/com/bookmie/gitforenv/GitforenvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmie.gitforenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitforenvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
